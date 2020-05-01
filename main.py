8dbb1c5943d68c1a44366951357f5b6e39f16ef1
manoloesparta <manoloesparta@gmail.com>
2020-05-01 00:05:23 -0700

    able to generate imgs

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from markov import Markov
 from PIL import Image
 
 def main():
-    img = Image.open('example.jpg')
+    img = Image.open('we.jpg')
     data = np.asarray(img)
 
-    c = Markov(data, 0.2)
+    c = Markov(data, 3)
     c.create_chain()
+    gen = c.generate_image()
+    x = Image.fromarray(gen, mode='RGB')
+    x.save('art.jpg')
     
-    print(c.chain)
-
 if __name__ == "__main__":
     main()
--- markov.py
+++ markov.py
@@ -1,34 +1,61 @@
 import numpy as np
 import random as rnd
 
+
 class Markov():
 
-    def __init__(self, img, per):
+    def __init__(self, img, ngram):
         self.img = img
-        self.per = per
+        self.ngram = ngram
         self.chain = {}
+        self.plain = [ k for i in img for j in i for k in j ]
+
 
     def create_chain(self):
-        plain = [ k for i in self.img for j in i for k in j ]
-
-        for i in range(0, len(plain), 3):
-            pix = Markov.pix_str(i, plain)
+        for i in range(0, len(self.plain), 3):
+            pix = Markov.pix_str(self.plain[i:i+3])
             
             if not pix in self.chain:
                 self.chain[pix] = []
             
-            self.chain[pix].append(Markov.pix_str(i+3, plain))
+            self.chain[pix].append(Markov.pix_str(self.plain[i+3:i+6]))
+
 
     def generate_image(self):
-        pass
+        start = rnd.choice(list(self.chain.keys()))
+        result = []
+
+        for _ in range(500 * 500):
+            possible = self.chain[start]
+            chosen = rnd.choice(possible)
+
+            if chosen == '':
+                chosen = rnd.choice(list(self.chain.keys()))
+            
+            start = chosen
+            result.append(Markov.pix_arr(start))
+
+        return self._to_matrix(result)
+
+
+    def _to_matrix(self, arr):
+        matrix = []
+        for i in range(500):
+            row = []
+            for j in range(500):
+                row.append(arr[(i * 500)+j])
+            matrix.append(row)
+        return np.array(matrix, dtype=np.uint8)
+
 
     @staticmethod
-    def pix_str(index, pixels):
-        p = map(lambda x: str(x), pixels[index:index+3])
+    def pix_str(pixels):
+        p = map(lambda x: str(x), pixels)
         return ','.join(p)
+
 
     @staticmethod
     def pix_arr(pix):
         p = pix.split(',')
-        p = map(lambda  x: int(x), pix)
+        p = map(lambda  x: int(x), p)
         return list(p)
